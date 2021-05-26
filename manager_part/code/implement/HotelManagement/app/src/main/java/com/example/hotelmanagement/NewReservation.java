c4a3172ea5700c002123694e59e26154349df194
kimjm9841 <kimjm9841@naver.com>
2021-05-27 05:49:09 +0900

    implement main menu

---
+++ manager_part/code/implement/HotelManagement/app/src/main/java/com/example/hotelmanagement/NewReservation.java
@@ -0,0 +1,12 @@
+package com.example.hotelmanagement;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewReservation extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_reservation);
+    }
+}
