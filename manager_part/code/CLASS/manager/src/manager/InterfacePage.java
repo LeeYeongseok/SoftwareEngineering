fcfaaeea62d9f3d5b0b1fb31b6c7d1b14f94f4b3
gamjaa <dlgodus21@gmail.com>
2021-05-26 20:32:36 +0900

    make class folder

---
+++ manager_part/code/CLASS/manager/src/manager/InterfacePage.java
@@ -0,0 +1,23 @@
+package manager;
+
+public class InterfacePage {
+	int menuChoice;
+	RoomInfo room = new RoomInfo();
+	int rsvDecision;
+	
+	private void enterMenu() {
+		//관리자로부터 menu 선택 정보 받아오기
+	}
+	
+	private void getRoomInfo(){
+	    // 관리자로부터 객실 정보 받아오기
+	}
+	 
+	private void getDecision(){
+	    // 관리자로부터 예약에 대한 accept or reject 정보 받아오기
+	}
+
+	private void displayPage(){
+	    // pageMaker로부터 받아온 page 정보를 화면에 띄움
+	}
+}
