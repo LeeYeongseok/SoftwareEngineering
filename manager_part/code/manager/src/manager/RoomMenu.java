f78f34b00ba8e84599f03b9219a5c749ce266db2
gamjaa <dlgodus21@gmail.com>
2021-05-26 19:52:13 +0900

    Add DB,controller,RoomMenu class

---
+++ manager_part/code/manager/src/manager/RoomMenu.java
@@ -0,0 +1,19 @@
+package manager;
+
+public class RoomMenu {
+	RoomInfo room;
+	int menu=0;
+	
+	// 사진은 object 형식??
+	public void add(RoomInfo roomInfo) {
+		room=roomInfo;
+		// 함수 pageMaker에 넘겨 줄 페이지 메뉴가 리턴값 -> 메소드 리턴형 int?
+	}
+	public void delete(int roomNum) {
+		
+	}
+	public void modify(RoomInfo roomInfo) {
+		room=roomInfo;
+	}
+	
+}
